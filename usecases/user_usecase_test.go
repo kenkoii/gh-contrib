9da301cb5e98dcaccfb8718bc34b49a6de73c96f
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-11-01 21:42:33 +0800

    Added getByUsername tests

--- usecases/user_usecase_test.go
+++ usecases/user_usecase_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/kenkoii/a-fis-gh-contrib/repositories"
 )
 
-func TestSave(t *testing.T) {
+func TestUsecase(t *testing.T) {
 	r := repositories.NewInMemoryUserRepository()
 	u := NewUserUsecase(r)
 
@@ -15,8 +15,23 @@ func TestSave(t *testing.T) {
 		Username: "kenkoii",
 		ImageUrl: "https://avatars0.githubusercontent.com/u/11359825?s=460&v=4",
 	}
-	_, err := u.Save(user)
-	if err != nil {
-		t.Errorf("Error saving, message: %s", err.Error())
-	}
+
+	t.Run("Save", func(t *testing.T) {
+		_, err := u.Save(user)
+		if err != nil {
+			t.Errorf("Error saving, message: %s", err.Error())
+		}
+	})
+
+	t.Run("GetByUsername", func(t *testing.T) {
+		want := "kenkoii"
+		got, err := u.GetByUsername(want)
+		if err != nil {
+			t.Errorf("No user fetched error: %s", err.Error())
+		}
+
+		if got.Username != want {
+			t.Errorf("Wrong user fetched: %s", err.Error())
+		}
+	})
 }
