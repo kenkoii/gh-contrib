e1090d5ac9272c6b48d4c2bee4a213e83978959b
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-11-03 12:41:33 +0800

    Added request struct

--- main.go
+++ main.go
@@ -15,6 +15,25 @@ func init() {
 	http.HandleFunc("/", e.ServeHTTP)
 }
 
+var url = `https://api.github.com/repos/%s/%s/commits?
+			access_token=%s
+			&since=%s
+			&until=%s
+			&author=%s`
+
 func mainHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World")
+}
+
+type ContribRequest struct {
+	UserOrg     string
+	Repo        string
+	Author      string
+	DateSince   string
+	DateUntil   string
+	AccessToken string
+}
+
+func fetchGithubContribs(ContribRequest) {
+
 }
